b32e30bd5d89c0fd41258f15aca6ac87c8a25a95
Nischal Bade <nischal.bade.13@gmail.com>
2022-06-13 14:10:40 +0545

    updatePart2.8

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,65 +2,88 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  const [newName, setNewName] = useState('')
-
-  const handleChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleAdd = (event) => {
-    event.preventDefault()
-    const  newObj =[ 
-      {name:newName}
-    ]
-    const compare = (o1,o2) => {
-      console.log("comp..",o1,o2,o1.name===o2.name)
-      if (o1.name===o2.name){
-        return true
-      }
-    }
-  
-    console.log(persons)
-    persons.map(person => {
-     console.log( compare(person,newObj))
-
-    })
-    
-    // if(compare){
-    //   console.log(newName)
-    //   alert(`${newName} is already added to phonebook`)
-    // }
-    // else{
-    
-    // setPersons(persons.concat(newObj))
-    // }
-    
-    setNewName('')
-  }
-
-
   return (
     <div>
       <h2>Phonebook</h2>
+      <Filter/>
+      <h3>Add a new</h3>
+      <PersonForm persons={persons} setPersons={setPersons}/>
+      <h3>Numbers</h3>
+      {persons.map(person => <Person name={person.name} number={person.number} key={person.id}/>)}
+    </div>
+  )
+}
+
+const Filter =() =>{
+  const[filter,setFilter]=useState('')
+  const handleFilterChange = () => {
+    setFilter(filter)
+  }
+  return(
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange}/>
+    </div>  
+  )
+}
+const Person = ({name, number}) => {
+  return(
+    <div>{name}  -  {number}</div>
+  )
+}
+
+const PersonForm = ({persons,setPersons}) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+    
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+    
+  }
+  const handleAdd = (event) => {
+    event.preventDefault()
+    const  newObj ={
+      name:newName,
+      number: newNumber,
+      id: persons.length+1
+    }
+    
+    const compare = (o1,o2) => {
+      if (o1.name===o2.name || o1.number===o2.number){
+        return true
+      }
+    }
+    (persons.some(person=> compare(person,newObj)))
+    ? alert (`${newName} is already added to phonebook`)
+    :setPersons(persons.concat(newObj))
+
+    
+    setNewName('')
+    setNewNumber('')
+  }
+  return(
+    <div>
       <form onSubmit={handleAdd}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit" >add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <Person name={person.name}/>)}
     </div>
   )
-}
-
-const Person = ({name}) => {
-  return(
-    <div>{name}</div>
-  )
-}
+} 
 export default App
