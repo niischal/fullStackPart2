69e8286363d382f528879f7db55a5d529f4c2cb0
Nischal Bade <nischal.bade.13@gmail.com>
2022-06-30 16:24:59 +0545

    Exercise 3.19 cmplete

--- phonebook/src/services/phonebookService.js
+++ phonebook/src/services/phonebookService.js
@@ -1,31 +1,25 @@
-import axios from "axios"
-const baseUrl='http://localhost:3001/persons'
+import axios from 'axios'
+const baseUrl = 'api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-const create = (newObject) => {
-    const request = axios.post(baseUrl,newObject)
-    return request.then(response => response.data)
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
-const deletePerson = (id) => {
-    const request = axios.delete(baseUrl +'/'+ id)
-    return request.then(response => response.data)
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
-const updateNumber = (id,newObj) => {
-    const request = axios.put(baseUrl+'/'+ id,newObj)
-    return request.then(response => response.data)
-}
-const phonebookService={
-    getAll:getAll, 
-    create:create, 
-    updateNumber:updateNumber,
-    deletePerson:deletePerson
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
-
-export default phonebookService
+export default { 
+  getAll, create, update, remove 
+}
